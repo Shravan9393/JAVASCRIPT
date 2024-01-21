90fce3149bedac4ba9835b93c970c17238a5d30b
Shravan Yadav <130574591+Shravan9393@users.noreply.github.com>
2024-01-21 04:14:35 +0000

    datatypes

---
+++ o1_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+// alert ( 3 + 3 ) // we are using nodejs,not brwser
+let name = "shravan"
+let age = 18
+let isLoggedIn = false
+
+// datatypes
+// number
+// string = ""
+// bigInt
+// boolean => true/false
+// null => standalone value

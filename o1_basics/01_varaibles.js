ecaace4d2fde94e358e880b2ee88dc1d7312d877
Shravan Yadav <130574591+Shravan9393@users.noreply.github.com>
2024-01-21 04:20:34 +0000

    datatypes and vaiables

---
+++ o1_basics/01_varaibles.js
@@ -0,0 +1,15 @@
+const accountId = 14434
+let accountEmail = "shravan@outlook.com"
+var accountPassword = "12345"
+
+// prefer not to use var, insted of it use let
+// beacause  of scope issue
+
+accountCity = "Varanasi"
+let accoutnState;
+// console.log(accountId);
+accountEmail = "wekuhwei@.com"
+accountPassword = "33q3431"
+accountCity = "delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accoutnState])
